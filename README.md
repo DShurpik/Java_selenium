b2d4f8115e811ed9c456f98533e1feda649d13c6
DShurpik <dimashurpik@gmail.com>
2025-03-31 16:58:34 +0200

    Refactor driver management to use ThreadLocal for parallel test execution; add retry mechanism for flaky tests

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Java_selenium
-1. для запуска тестов, -DsuiteXml= имя.xml и -Dconfig=имя проперти файла
+1. To start test use a command mvn clean test -DsuiteXml="suite name" -Dconfig="property name"
+2. Create a driver factory for creating a driver
+3. Realize that it's possible to change the browser in terminal by using the command -Dbrowser="browser name".
+   Default browser is chrome.
+   - Example: mvn clean test -DsuiteXml="suite name" -Dconfig="property name" -Dbrowser="firefox"
+   - Available browsers: chrome, firefox
+4. Realize possibility to make an allure report by using the command mvn allure:report or mvn allure:serve
+5. Add a logger, can read logs from the console and from the file target/logs/log.out
+6. Add a retry mechanism for flaky tests
+7. Add a screenshot for failed tests
+8. Add ThreadLocal for driver for possibility to run tests in parallel
--- src/main/java/basePages/BaseTest.java
+++ src/main/java/basePages/BaseTest.java
@@ -1,12 +1,9 @@
 package basePages;
 
-import driver.DriverManager;
 import driver.DriverManagerFactory;
 import listeners.InvokedMethodListener;
 import listeners.ListenerForProperty;
-import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -20,23 +17,19 @@ import static driver.DriverManager.getWebDriver;
 
 public abstract class BaseTest {
 
-    @Getter
-    protected WebDriver driver;
-
     @BeforeMethod
     public void setUp() {
         // Получаем браузер с учетом -Dbrowser= и значения из properties
         String browser = PropertyReader.getProperty("browser", "chrome").toUpperCase();
 
         // Передаем правильное значение в фабрику DriverManagerFactory
-        DriverManager driverManager = DriverManagerFactory.getManager(browser);
-        driver = getWebDriver();
+        DriverManagerFactory.getManager(browser).createDriver();
         log.debug("New web driver is set up");
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
+        if (getWebDriver() != null) {
             closeDriver();
         }
         log.info("Web driver is closed");
