c7282216beb28dc4a440870b3edd94ef74506e64
DShurpik <dimashurpik@gmail.com>
2025-05-17 19:35:09 +0200

    Enhance ChromeDriver creation; set window size for headless mode and clean up YAML configuration

--- src/main/java/driver/ChromeDriverCreator.java
+++ src/main/java/driver/ChromeDriverCreator.java
@@ -1,6 +1,7 @@
 package driver;
 
 import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -14,9 +15,10 @@ public class ChromeDriverCreator extends DriverCreator {
         log.info("Creating ChromeDriver");
         ChromeOptions options = new ChromeOptions();
         if (Boolean.parseBoolean(PropertyReader.getProperty("browser.headless", "false"))) {
-            options.addArguments("--headless", "--disable-gpu", "--window-size=1920x1080");
+            options.addArguments("--headless", "--disable-gpu");
         }
         WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().setSize(new Dimension(1920, 1080));
         configure(driver);
         return driver;
     }
