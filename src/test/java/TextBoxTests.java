fa5ff95505ab0b85b02067e306e9617435f90922
DShurpik <105069264+DShurpik@users.noreply.github.com>
2025-09-19 17:38:19 +0200

    Add fillFieldByFaker test; utilize Faker library for dynamic data input in TextBoxTests (#12)

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,4 +1,5 @@
 import basePages.BaseTest;
+import dataGenerator.DataUserGenerator;
 import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -21,6 +22,36 @@ public class TextBoxTests extends BaseTest {
         String userPermanentAddress = getProperties().getProperty("permanentAddress");
 
         TextBoxPage textBoxPage = new TextBoxPage();
+        textBoxPage.open(getProperties().getProperty("url"));
+        textBoxPage.navigateTo(ELEMENTS);
+        textBoxPage.navigateToMenu(TEXT_BOX);
+
+        textBoxPage
+                .fillFullNameField(userFullName)
+                .fillEmailField(userEmail)
+                .fillCurrentAddressField(userCurrentAddress)
+                .fillPermanentAddressField(userPermanentAddress);
+        textBoxPage.clickSubmitBtn();
+
+        Assert.assertEquals(textBoxPage.getNameText(), userFullName, "User name is not correct");
+        Assert.assertEquals(textBoxPage.getEmailText(), userEmail, "User email is not correct");
+        Assert.assertEquals(textBoxPage.getCurrentAddressText(), userCurrentAddress, "User current address is not correct");
+        Assert.assertEquals(textBoxPage.getPermanentAddressText(), userPermanentAddress, "User permanent address is not correct");
+    }
+
+    @Owner("John Doe")
+    @Severity(SeverityLevel.NORMAL)
+    @TmsLink("TC-2")
+    @Story("Filling fields by values from Faker library")
+    @Test(description = "Using data from Faker library to fill the user's data fields")
+    public void fillFieldByFaker() {
+        TextBoxPage textBoxPage = new TextBoxPage();
+        DataUserGenerator user = new DataUserGenerator();
+        String userFullName = user.getFullName();
+        String userEmail = user.getEmail();
+        String userCurrentAddress = user.getCurrentAddress();
+        String userPermanentAddress = user.getPermanentAddress();
+
         textBoxPage.open(getProperties().getProperty("url"));
         textBoxPage.navigateTo(ELEMENTS);
         textBoxPage.navigateToMenu(TEXT_BOX);
