52b1c0f2f54bffc4b3f729a8d7560da3e6acde86
DShurpik <105069264+DShurpik@users.noreply.github.com>
2025-09-20 19:28:48 +0200

    Add fillFieldByInvalidEmail test; utilize RandomStringGenerator for dynamic email input in TextBoxTests (#13)

--- src/main/java/dataGenerator/RandomStringGenerator.java
+++ src/main/java/dataGenerator/RandomStringGenerator.java
@@ -3,13 +3,13 @@ package dataGenerator;
 import java.util.Random;
 
 public class RandomStringGenerator {
-    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    public static String generateRandomString() {
+    public String generateRandomString() {
         Random random = new Random();
         int length = random.nextInt(1, 25);
         StringBuilder result = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
+            String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             int index = (int) (Math.random() * ALPHABET.length());
             result.append(ALPHABET.charAt(index));
         }
