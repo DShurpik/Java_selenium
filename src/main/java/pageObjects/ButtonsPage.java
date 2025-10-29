c96e1a06cd9e2534c7838f64dc999f62cc87e1e1
DShurpik <dimashurpik@gmail.com>
2025-10-29 16:48:43 +0100

    Refactor ButtonsPage and add ButtonsTest for improved click actions; implement retry logic for double and right click actions

--- src/main/java/pageObjects/ButtonsPage.java
+++ src/main/java/pageObjects/ButtonsPage.java
@@ -3,19 +3,21 @@ package pageObjects;
 import basePages.BasePage;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 @Log4j2
 public class ButtonsPage extends BasePage {
 
-    @FindBy(id = "doubleClickBtn")
+    @FindBy(xpath = "//button[@class='btn btn-primary' and text()='Double Click Me']")
     private WebElement doubleClickBtn;
 
-    @FindBy(id = "rightClickBtn")
+    @FindBy(xpath = "//button[@class='btn btn-primary' and text()='Right Click Me']")
     private WebElement rightClickBtn;
 
-    @FindBy(xpath = "//button[text()='Click Me']")
+    @FindBy(xpath = "//button[@class='btn btn-primary' and text()='Click Me']")
     private WebElement clickMeBtn;
 
     @FindBy(id = "doubleClickMessage")
@@ -27,41 +29,67 @@ public class ButtonsPage extends BasePage {
     @FindBy(id = "dynamicClickMessage")
     private WebElement dynamicClickMessageField;
 
-    @Step("Click on 'Double Click Me'")
-    public void doubleClick() {
-        log.info("Click on Double Click Me button");
-
-        actions.doubleClick(doubleClickBtn).perform();
-    }
-
     @Step("Get double click result")
     public String doubleClickResult() {
         log.info("Get double click result text");
-        return doubleClickMessageField.getText();
+        return getText(doubleClickMessageField);
     }
 
-    @Step("Click on 'Right Click Me'")
+    @Step("Double click on 'Double Click Me' button")
+    public void doubleClick() {
+        executeActionWithRetry(doubleClickBtn, "Double Click Me", "doubleClick");
+    }
+
+    @Step("Right click on 'Right Click Me' button")
     public void rightClick() {
-        log.info("Click on Right click button");
-
-        actions.contextClick(rightClickBtn).perform();
+        executeActionWithRetry(rightClickBtn, "Right Click Me", "rightClick");
     }
+
+
+    @Step("Click on {0} with {1} action")
+    private void executeActionWithRetry(WebElement webElement, String actionName, String actionType) {
+        log.info("Performing {} on {}", actionType, actionName);
+        int attempts = 0;
+        while (attempts < 3) {
+            try {
+                wait.until(ExpectedConditions.elementToBeClickable(webElement));
+                if (actionType.equals("doubleClick")) {
+                    actions.doubleClick(webElement).perform();
+                } else if (actionType.equals("rightClick")) {
+                    actions.contextClick(webElement).perform();
+                }
+                log.info("{} performed successfully", actionType);
+                return;
+            } catch (StaleElementReferenceException e) {
+                log.warn("StaleElementReferenceException caught during {}, retrying... Attempt: {}", actionType, attempts + 1);
+                attempts++;
+                if (attempts == 3) {
+                    log.error("Failed to perform {} after 3 attempts", actionType);
+                    throw e;
+                }
+            } catch (Exception e) {
+                log.error("Error performing {}: {}", actionType, e.getMessage());
+                throw e;
+            }
+        }
+    }
+
 
     @Step("Get right click result")
     public String rightClickResult() {
         log.info("Get right click result text");
-        return rightClickMessageField.getText();
+        return getText(rightClickMessageField);
     }
 
     @Step("Click on 'Click Me'")
     public void clickMe() {
         log.info("Click on Click button");
-        clickMeBtn.click();
+        click(clickMeBtn);
     }
 
     @Step("Get click me result")
     public String clickGetResult() {
         log.info("Get click me result text");
-        return dynamicClickMessageField.getText();
+        return getText(dynamicClickMessageField);
     }
 }
---
+++ src/test/java/ButtonsTest.java
@@ -0,0 +1,34 @@
+import basePages.BaseTest;
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pageObjects.ButtonsPage;
+
+import static pageObjects.Navigation.BUTTONS;
+import static pageObjects.Navigation.ELEMENTS;
+
+public class ButtonsTest extends BaseTest {
+
+    @Owner("John Doe")
+    @Severity(SeverityLevel.NORMAL)
+    @TmsLink("FORM-TC-014")
+    @Story("Mouse clicks")
+    @Test(description = "Different kind of clicks")
+    public void doubleRightMouseClicksTest() {
+        ButtonsPage buttonsPage = new ButtonsPage();
+
+        buttonsPage.open();
+        buttonsPage.navigateTo(ELEMENTS);
+        buttonsPage.navigateToMenu(BUTTONS);
+
+        buttonsPage.doubleClick();
+        buttonsPage.rightClick();
+        buttonsPage.clickMe();
+
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(buttonsPage.doubleClickResult(), "You have done a double click");
+        softAssert.assertEquals(buttonsPage.rightClickResult(), "You have done a right click");
+        softAssert.assertEquals(buttonsPage.clickGetResult(), "You have done a dynamic click");
+        softAssert.assertAll();
+    }
+}
