ce68f871e116874b124485c77aa3d01c6b82b9fa
DShurpik <dimashurpik@gmail.com>
2025-12-19 15:24:24 +0100

    Add FramesPage and FramesTests for frame handling; implement methods to switch between frames and validate frame visibility in tests

---
+++ src/test/java/FramesTests.java
@@ -0,0 +1,50 @@
+import basePages.BaseTest;
+import io.qameta.allure.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.FramesPage;
+import pageObjects.Navigation.*;
+
+import static pageObjects.Navigation.ALERTS;
+import static pageObjects.Navigation.FRAMES;
+
+public class FramesTests extends BaseTest {
+
+    @Owner("John Doe")
+    @Severity(SeverityLevel.NORMAL)
+    @TmsLink("FORM-TC-026")
+    @Story("Check that frame is visible and switchable")
+    @Test(description = "Check, that frame is visible and switchable")
+    public void frame1Test() {
+        FramesPage framesPage = new FramesPage();
+        framesPage.open();
+        framesPage.navigateTo(ALERTS);
+        framesPage.navigateToMenu(FRAMES);
+
+        framesPage.switchToFrame1();
+
+        Assert.assertEquals(
+                framesPage.getFrameHeaderText(),
+                "This page could not be found.",
+                "Frame 1 header text does not match expected value");
+    }
+
+    @Owner("John Doe")
+    @Severity(SeverityLevel.NORMAL)
+    @TmsLink("FORM-TC-027")
+    @Story("Check that frame 2 is visible and switchable")
+    @Test(description = "Check, that frame 2 is visible and switchable")
+    public void frame2Test() {
+        FramesPage framesPage = new FramesPage();
+        framesPage.open();
+        framesPage.navigateTo(ALERTS);
+        framesPage.navigateToMenu(FRAMES);
+
+        framesPage.switchToFrame2();
+
+        Assert.assertEquals(
+                framesPage.getFrameHeaderText(),
+                "This page could not be found.",
+                "Frame 2 header text does not match expected value");
+    }
+}
